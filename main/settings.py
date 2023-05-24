623fd62f43efd413a9195786bae7191344d1fa41
Saviour Bassey <saviourbazi2021@yahoo.com>
2023-05-24 21:26:34 +0100

    added the hosted url among allowed host

--- main/settings.py
+++ main/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', "web-production-8d52.up.railway.app"]
+ALLOWED_HOSTS = ['127.0.0.1', "ft9ja.onrender.com"]
 
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1", "https://web-production-8d52.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1", " https://ft9ja.onrender.com"]
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
